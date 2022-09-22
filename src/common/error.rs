819128dfbb48c0de8ef168af34131c51d3c9c230
evanzp0 <564374058@qq.com>
2022-09-23 06:34:47 +0800

    update

--- src/common/error.rs
+++ src/common/error.rs
@@ -1,8 +1,13 @@
+use std::error::Error;
+use std::fmt::{Display, Formatter};
+
 use axum::Json;
 use axum::http::StatusCode;
 use axum::response::{IntoResponse, Response, Html};
-use ractiviti_core::error::AppError;
+use color_eyre::Report;
+use ractiviti_core::error::{AppError, ErrorCode};
 use axum::http::{HeaderValue, HeaderMap};
+use serde::Serialize;
 use serde_json::json;
 use log4rs_macros::error;
 
@@ -18,31 +23,6 @@ pub enum WebError {
 pub enum ErrBody<'a> {
     Json(&'a str),
     Html(&'a str),
-}
-
-/// This makes it possible to use `?` to automatically convert a `AppError`
-/// into an `WebError`.
-impl From<AppError> for WebError {
-    fn from(inner: AppError) -> Self {
-        WebError::AppError(inner)
-    }
-}
-
-impl IntoResponse for WebError {
-    fn into_response(self) -> Response {
-
-        let rst = match &self {
-            WebError::AppError(ae) => match ae.code {
-                _ => {
-                    let trace_no = gen_random_str(16);
-                    error!("trace_no: {}, {:?}", trace_no, ae);
-                    err_with_trace_no(StatusCode::INTERNAL_SERVER_ERROR, Some(&trace_no), None)
-                },
-            }
-        };
-
-        rst.into_response()
-    }
 }
 
 pub fn err_with_trace_no(status_code: StatusCode, trace_no: Option<&str>, body: Option<ErrBody>) -> impl IntoResponse {
@@ -71,3 +51,46 @@ pub fn err_with_trace_no(status_code: StatusCode, trace_no: Option<&str>, body: 
         (status_code, headers).into_response()
     }
 }
+
+impl IntoResponse for WebError {
+    fn into_response(self) -> Response {
+
+        let rst = match &self {
+            WebError::AppError(ae) => match ae.code {
+                _ => {
+                    let trace_no = gen_random_str(16);
+                    error!("trace_no: {}, {:?}", trace_no, ae.to_string());
+                    err_with_trace_no(StatusCode::INTERNAL_SERVER_ERROR, Some(&trace_no), None)
+                },
+            }
+        };
+
+        rst.into_response()
+    }
+}
+
+/// This makes it possible to use `?` to automatically convert a `AppError`
+/// into an `WebError`.
+impl From<AppError> for WebError {
+    fn from(inner: AppError) -> Self {
+        WebError::AppError(inner)
+    }
+}
+
+impl From<Report> for WebError {
+    fn from(inner: Report) -> Self {
+        let ae = AppError::new(ErrorCode::InternalError, None, "", Some(Box::new(WrappedError(inner.to_string()))));
+        WebError::AppError(ae)
+    }
+}
+
+#[derive(Debug, Serialize)]
+struct WrappedError(String);
+
+impl Display for WrappedError {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", &self.0)
+    }
+}
+
+impl Error for WrappedError {}
--- src/handles/root.rs
+++ src/handles/root.rs
@@ -1,12 +1,19 @@
 use axum::response::{IntoResponse, Html};
+use ractiviti_core::error::AppError;
 
 use crate::common::WebResult;
+use color_eyre::Result;
 
 pub async fn root() -> WebResult<impl IntoResponse>
 {
     // err_with_trace_no(StatusCode::INTERNAL_SERVER_ERROR, Some("123"), None)
     // let ae = AppError::internal_error("line1");
     // Err(ae)?
-    
+    let a = gen_err()?;
+
     Ok(Html("<h1>Hello, World!</h1>"))
+}
+
+fn gen_err() -> Result<String> {
+    Err(AppError::unexpected_error("location1"))?
 }
