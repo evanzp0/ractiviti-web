a3b437ffceb65c68c409870808afa2e9759e2cef
evanzp0 <564374058@qq.com>
2022-11-02 23:38:17 +0800

    update

--- web/modules/change_password/container/change_password.tsx
+++ web/modules/change_password/container/change_password.tsx
@@ -1,134 +1,128 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import Grid from '@mui/material/Unstable_Grid2/Grid2';
-import Container from '@mui/material/Container';
 import { Box, Button, TextField } from '@mui/material';
-import IVerifyPassword from '../model/verify_password';
 import IPasswordData from '../model/password_data';
+import { object, string, TypeOf } from 'zod';
+import { SubmitHandler, useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { LoadingButton } from '@mui/lab';
 
 const theme = createTheme();
 
-let initStateDate : IPasswordData & IVerifyPassword = {
-    password: '',
-    new_password: '',
-    re_password: '',
-    is_valid: null,
-    err_field: '',
-    err_message: '',
-}
+const passwordSchema = object({
+    password: string()
+        .min(1, '密码不可为空')
+        .min(5, '密码长度不能小于5位数')
+        .max(20, '密码长度不可超过20个'),
+    new_password: string()
+        .min(1, '密码不可为空')
+        .min(5, '密码长度不能小于5位数')
+        .max(20, '密码长度不可超过20个'),
+    re_password: string()
+        .min(1, '密码不可为空')
+        .min(5, '密码长度不能小于5位数')
+        .max(20, '密码长度不可超过20个'),
+}).refine((data) => data.new_password === data.re_password, {
+    path: ['re_password'],
+    message: '两次输入的密码不一致',
+});
+
+type PasswordInput = TypeOf<typeof passwordSchema>;
 
 export default function ChangePassword() {
-    const [state, setState] = useState(initStateDate); 
+    const [loading, setLoading] = useState(false);
+    
+    const {
+        register,
+        formState: { errors, isSubmitSuccessful },
+        reset,
+        setError,
+        handleSubmit,
+    } = useForm<PasswordInput>({
+        resolver: zodResolver(passwordSchema),
+    });
 
-    const handleChangePassword = (event: React.FormEvent<HTMLFormElement>) => {
+    useEffect(() => {
+        if (isSubmitSuccessful) {
+          reset();
+        }
+      }, [isSubmitSuccessful]);
+
+    const handleChangePassword: SubmitHandler<PasswordInput> = (values) => {
+        // console.log(values);
+        // setLoading(true);
+        // setError('password', { type: 'focus', message: '当前密码不正确' });
+    };
+    // console.log(errors);
+
+    const handleReset = (event: React.MouseEvent<HTMLElement>) => {
         event.preventDefault();
-        const data = new FormData(event.currentTarget);
-
-        let state_data: IPasswordData & IVerifyPassword = {
-            password: data.get('password')!.toString().trim(),
-            new_password: data.get('new_password')!.toString().trim(),
-            re_password: data.get('re_password')!.toString().trim(),
-            is_valid: null,
-            err_field: '',
-            err_message: '',
-        };
-
-        let err_empty = '密码不可为空';
-
-        let err_len = '密码长度不能小于5位数';
-        if (state_data.password.length < 5) {
-            state_data.err_field = 'password';
-            state_data.err_message = err_len;
-            state_data.is_valid = false;
-        } else if (state_data.new_password.length < 5) {
-            state_data.err_field = 'new_password';
-            state_data.err_message = err_len;
-            state_data.is_valid = false;
-        } else if (state_data.re_password.length < 5) {
-            state_data.err_field = 're_password';
-            state_data.err_message = err_len;
-            state_data.is_valid = false;
-        } else if (state_data.new_password != state_data.re_password) {
-            state_data.err_field = 're_password';
-            state_data.err_message = '两次密码输入不一致';
-            state_data.is_valid = false;
-        } else {
-            state_data.is_valid = false;
-        }
-
-        if (!state_data.is_valid) {
-            setState(state_data);
-        }
+        reset();
     };
 
     return (
         <ThemeProvider theme={theme}>
-            <Container component="main" maxWidth="xs">
-                <Box component="form" onSubmit={handleChangePassword} noValidate={false} sx={{ mt: 1 }}>
-                    <TextField
-                        margin="normal"
-                        required
-                        fullWidth
-                        name="password"
-                        label="当前密码"
-                        type="password"
-                        id="password"
-                        value={state.password}
-                        onChange={(e) => setState({...state, password: e.target.value})}
-                        error={!state.is_valid && state.err_field == 'password' ? true : false}
-                        helperText={!state.is_valid && state.err_field == 'password' ? state.err_message : null}
-                    />
-                    <TextField
-                        margin="normal"
-                        required
-                        fullWidth
-                        name="new_password"
-                        label="新密码"
-                        type="password"
-                        id="new_password"
-                        value={state.new_password}
-                        onChange={(e) => setState({...state, new_password: e.target.value})}
-                        error={!state.is_valid && state.err_field == 'new_password' ? true : false}
-                        helperText={!state.is_valid && state.err_field == 'new_password' ? state.err_message : null}
-                    />
-                    <TextField
-                        margin="normal"
-                        required
-                        fullWidth
-                        name="re_password"
-                        label="再次输入"
-                        type="password"
-                        id="re_password"
-                        value={state.re_password}
-                        onChange={(e) => setState({...state, re_password: e.target.value})}
-                        error={!state.is_valid && state.err_field == 're_password' ? true : false}
-                        helperText={!state.is_valid && state.err_field == 're_password' ? state.err_message : null}
-                    />
-                    <Grid container spacing={2} >
-                        <Grid xs={6}>
-                            <Button
-                                type="submit"
-                                fullWidth
-                                variant="contained"
-                                sx={{ mt: 3, mb: 2 }}
-                            >
-                                确定
-                            </Button>
-                        </Grid>
-                        <Grid xs={6}>
-                            <Button
-                                type="button"
-                                fullWidth
-                                variant="contained"
-                                sx={{ mt: 3, mb: 2, backgroundColor:"gray" }}
-                            >
-                                重置
-                            </Button>
-                        </Grid>
-                </Grid>
-                </Box>
-            </Container>
+            <Box component="form" onSubmit={handleSubmit(handleChangePassword)} noValidate sx={{maxWidth: '30rem' }} >
+                <TextField
+                    margin="normal"
+                    required
+                    fullWidth
+                    label="当前密码"
+                    type="password"
+                    id="password"
+                    error={!!errors['password']}
+                    helperText={errors['password'] ? errors['password'].message : ''}
+                    {...register('password')}
+                />
+                <TextField
+                    margin="normal"
+                    required
+                    fullWidth
+                    label="新密码"
+                    type="password"
+                    id="new_password"
+                    error={!!errors['new_password']}
+                    helperText={errors['new_password'] ? errors['new_password'].message : ''}
+                    {...register('new_password')}
+                />
+                <TextField
+                    margin="normal"
+                    required
+                    fullWidth
+                    label="再次输入"
+                    type="password"
+                    id="re_password"
+                    error={!!errors['re_password']}
+                    helperText={errors['re_password'] ? errors['re_password'].message : ''}
+                    {...register('re_password')}
+                />
+                <Grid container spacing={2} >
+                    <Grid xs={6}>
+                        <LoadingButton
+                            variant='contained'
+                            fullWidth
+                            type='submit'
+                            loading={loading}
+                            sx={{ mt: 3, mb: 2 }}
+                        >
+                            提交
+                        </LoadingButton>
+                    </Grid>
+                    <Grid xs={6}>
+                        <Button
+                            type="button"
+                            fullWidth
+                            variant="contained"
+                            sx={{ mt: 3, mb: 2, backgroundColor:"gray" }}
+                            onClick={handleReset}
+                        >
+                            重置
+                        </Button>
+                    </Grid>
+            </Grid>
+
+            </Box>
         </ThemeProvider>
     )
 }
-    
--- web/modules/change_password/model/verify_password.ts
+++
@@ -1,5 +0,0 @@
-export default interface IVerifyPassword {
-    is_valid: boolean | null,
-    err_field: string,
-    err_message: string,
-}
