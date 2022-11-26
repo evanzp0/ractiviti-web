82bc144e368910a948a4d672719ec6772b614bfd
evanzp0 <564374058@qq.com>
2022-11-26 11:53:21 +0800

    update

--- web/common/component/data_grid.tsx
+++ web/common/component/data_grid.tsx
@@ -5,6 +5,7 @@ import { GridInitialStatePro } from "x-data-grid/src/models/gridStatePro";
 // import { DataGrid} from "@mui/x-data-grid";
 // import { DataGridPro } from "@mui/x-data-grid-pro";
 import { DataGridPro } from './mui_pro/data_grid_pro/DataGridPro'
+import { Box, MenuItem, Select, Typography } from "@mui/material";
 
 export default function PageDataGrid(props: PageDataGridProps) {
     return <DataGridPro
@@ -17,12 +18,30 @@ export default function PageDataGrid(props: PageDataGridProps) {
         pagination={true}
         components={{
             Pagination: () => 
+            <Box sx={{display: "flex", width: "100%", justifyContent: "space-between", alignItems: "center"}}>
+                <Box sx={{display: "flex", alignItems: "center"}}>
+                    <Typography variant="button" display="block" ml={2} mr={1}>
+                        共 103 条记录，每页
+                    </Typography>
+                    <Select
+                        id="page_size"
+                        value={10}
+                        size="small"
+                        sx={{width: 70, height:30}}
+                        onChange={()=>{}}
+                    >
+                        <MenuItem value={10}>10</MenuItem>
+                        <MenuItem value={20}>20</MenuItem>
+                        <MenuItem value={50}>30</MenuItem>
+                    </Select>
+                </Box>
                 <Pagination
                     color="primary"
                     page={props.page || 0}
                     count={props.count || 0}
                     onChange={props.onChange || function(){}}
                 />
+            </Box>
         }}
         sx={{
             '& .MuiDataGrid-footerContainer': {
